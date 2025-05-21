0ec0b4a5dff7c353b3f4a1bd8f07c5db954c0eb6
Sameer Ali <140313541+devxsameer@users.noreply.github.com>
2025-05-22 03:56:07 +0530

    style: :sparkles: Make The Game Ez2Play And Stylish

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const result = document.querySelector(".result");
+const choices = document.querySelectorAll(".choices-container img");
+const userScoreText = document.querySelector(".user-score span");
+const computerScoreText = document.querySelector(".computer-score span");
+let humanScore = 0,
+  computerScore = 0;
+
 function getComputerChoice() {
   let randomNum = Math.floor(Math.random() * 3);
   if (randomNum == 0) {
@@ -9,55 +16,50 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  return window.prompt("Enter Your Choice!");
-}
-
-let humanScore = 0,
-  computerScore = 0;
-
 const playRound = function (humanChoice, computerChoice) {
-  humanChoice = humanChoice.toLowerCase();
   if (humanChoice == "rock" && computerChoice == "paper") {
-    console.log("You Lose! Paper beats Rock");
+    result.innerHTML = "You Lose! Paper beats Rock";
     computerScore++;
   } else if (humanChoice == "rock" && computerChoice == "scissors") {
-    console.log("You Win! Rock beats Scissors");
+    result.innerHTML = "You Win! Rock beats Scissors";
     humanScore++;
   } else if (humanChoice == "paper" && computerChoice == "scissors") {
-    console.log("You Lose! Scissors beats Paper");
+    result.innerHTML = "You Lose! Scissors beats Paper";
     computerScore++;
   } else if (humanChoice == "paper" && computerChoice == "rock") {
-    console.log("You Win! Paper beats Rock");
+    result.innerHTML = "You Win! Paper beats Rock";
     humanScore++;
   } else if (humanChoice == "scissors" && computerChoice == "rock") {
-    console.log("You Lose! Rock beats Scissors");
+    result.innerHTML = "You Lose! Rock beats Scissors";
     computerScore++;
   } else if (humanChoice == "scissors" && computerChoice == "paper") {
-    console.log("You Win! Scissors beats Paper");
+    result.innerHTML = "You Win! Scissors beats Paper";
     humanScore++;
   } else {
     if (humanChoice == "rock") {
-      console.log("Its a Draw! Rock vs Rock");
+      result.innerHTML = "Its a Draw! Rock vs Rock";
     } else if (humanChoice == "scissors") {
-      console.log("Its a Draw! Scissors vs Scissors");
+      result.innerHTML = "Its a Draw! Scissors vs Scissors";
     } else {
-      console.log("Its a Draw! Paper vs Paper");
+      result.innerHTML = "Its a Draw! Paper vs Paper";
     }
   }
 };
-const playGame = () => {
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
+const triggerClick = (e) => {
+  playRound(e.target.id, getComputerChoice());
+  if (humanScore == 5 || computerScore == 5) {
+    if (humanScore > computerScore) {
+      window.alert(
+        `You Won The Game! Score: ${humanScore} vs ${computerScore}`
+      );
+    } else if (humanScore < computerScore) {
+      window.alert(
+        `You Lost The Game! Score: ${humanScore} vs ${computerScore}`
+      );
+    }
+    humanScore = computerScore = 0;
+  }
+  userScoreText.innerText = humanScore;
+  computerScoreText.innerText = computerScore;
 };
-playGame();
-if (humanScore > computerScore) {
-  console.log(`You Won The Game! Score: ${humanScore} vs ${computerScore}`);
-} else if (humanScore < computerScore) {
-  console.log(`You Lost The Game! Score: ${humanScore} vs ${computerScore}`);
-} else {
-  console.log(`The Game is A Draw! Score: ${humanScore} vs ${computerScore}`);
-}
+choices.forEach((choice) => choice.addEventListener("click", triggerClick));
